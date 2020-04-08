ecd99e91a49c858ad9f416bdb2968cf1ed8a233f
shivam sharma <shivam.sh9557@gmail.com>
2020-04-08 15:36:22 +0530

    2nd

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-import sqlite3
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
